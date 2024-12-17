20ee47f4482e17d89134280d2ae79a1ce9769fbd
Keks <keks@htmlacademy.ru>
2024-12-17 16:00:42 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/bigPicture.js
+++ 8/js/bigPicture.js
@@ -1,33 +1,44 @@
-export function bigPictureCloseInit(){
-  const bigPicture = document.querySelector(".big-picture");
-  const bigPictureCancel = bigPicture.querySelector(".big-picture__cancel");
-
-  function closeBigPicture(){
-    bigPicture.classList.add("hidden");
-    document.body.classList.remove("modal-open");
-  }
-  document.querySelector(".big-picture").classList.remove("hidden");
-  document.body.classList.add("modal-open");
-  bigPictureCancel.addEventListener("click", closeBigPicture);
-  document.addEventListener("keydown", (e)=>{
-    if (e.key === "Escape"){
-      closeBigPicture();
-    }
-  });
-}
-
 export function updateBigPicture(url, description, comments, likes){
+  const bigPicture = document.querySelector(".big-picture");
   const bigPictureImg = document.querySelector(".big-picture__img img");
   const bigPictureLikes = document.querySelector(".likes-count");
   const bigPictureCommentCounter = document.querySelector(".comments-count");
   const bigPictureDescription = document.querySelector(".social__caption");
+  const bigPictureCancel = bigPicture.querySelector(".big-picture__cancel");
 
   bigPictureImg.src = url;
   bigPictureLikes.textContent = likes;
   bigPictureCommentCounter.textContent = comments.length;
   bigPictureDescription.textContent = description;
-  updateComments([...comments]);
+
+  const renderedComments = comments.slice(0, 5);
+  const outstandingComments = comments.slice(5);
+  const loader = () => loadCommentsHandler(renderedComments, outstandingComments, loader);
+  updateComments(renderedComments, outstandingComments, loader);
+
+  document.querySelector(".big-picture").classList.remove("hidden");
+  document.body.classList.add("modal-open");
+  bigPictureCancel.addEventListener("click", closeBigPicture);
+  document.addEventListener("keydown", closeBigPictureByKey);
+
+  function closeBigPicture(){
+    const commentsLoader = document.querySelector(".comments-loader");
+
+    bigPicture.classList.add("hidden");
+    document.body.classList.remove("modal-open");
+
+    bigPictureCancel.removeEventListener("click", closeBigPicture);
+    document.removeEventListener("keydown", closeBigPictureByKey);
+    commentsLoader.removeEventListener("click", loader);
+  }
+
+  function closeBigPictureByKey(e){
+    if (e.key === "Escape"){
+      closeBigPicture();
+    }
+  }
 }
+
 
 function renderComments(comments){
   const commentList = document.querySelector(".social__comments");
@@ -50,26 +61,11 @@ function renderCommentsCounter(renderedComments){
 }
 
 
-function updateComments(comments) {
+function updateComments(renderedComments, outstandingComments, loader) {
   const displayedComments = document.querySelector(".social__comment-count");
   const commentsLoader = document.querySelector(".comments-loader");
 
-  const renderedComments = comments.slice(0, 5);
-  const outstandingComments = comments.slice(5);
-
-  function loadCommentsHandler(){
-    renderedComments.push(...outstandingComments.slice(0, 5));
-    outstandingComments.splice(0, 5);
-    renderComments(renderedComments);
-    renderCommentsCounter(renderedComments);
-
-    if (outstandingComments.length === 0) {
-      commentsLoader.classList.add("hidden");
-      commentsLoader.removeEventListener("click", loadCommentsHandler);
-    }
-  }
-
-  commentsLoader.addEventListener("click", loadCommentsHandler);
+  commentsLoader.addEventListener("click", loader);
 
   displayedComments.classList.remove("hidden");
   commentsLoader.classList.remove("hidden");
@@ -79,6 +75,19 @@ function updateComments(comments) {
 
   if (outstandingComments.length === 0) {
     commentsLoader.classList.add("hidden");
-    commentsLoader.removeEventListener("click", loadCommentsHandler);
+    commentsLoader.removeEventListener("click", loader);
+  }
+}
+
+function loadCommentsHandler(renderedComments, outstandingComments, loader){
+  const commentsLoader = document.querySelector(".comments-loader");
+  renderedComments.push(...outstandingComments.slice(0, 5));
+  outstandingComments.splice(0, 5);
+  renderComments(renderedComments);
+  renderCommentsCounter(renderedComments);
+
+  if (outstandingComments.length === 0) {
+    commentsLoader.classList.add("hidden");
+    commentsLoader.removeEventListener("click", loader);
   }
 }
